d139d504b32f0b8790315bc6e4af85e1eb376abb
Fáber Sándor <sandorwow@gmail.com>
2025-06-22 14:18:36 +0200

    Create database.h
    Signed-off-by: Fáber Sándor <sandorwow@gmail.com>

---
+++ include/database.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <sqlite3.h>
+#include <string>
+
+class Database {
+public:
+    Database(const std::string& path);
+    ~Database();
+    
+    bool execute(const std::string& sql); // SQL parancs végrehajtása
+    
+private:
+    sqlite3* db = nullptr;
+};
