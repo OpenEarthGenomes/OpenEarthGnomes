c97c7f5b3e01184a027cd168341b8978d145c919
Fáber Sándor <sandorwow@gmail.com>
2025-06-22 14:52:39 +0200

    Update database.cpp
    Signed-off-by: Fáber Sándor <sandorwow@gmail.com>

--- src/database.cpp
+++ src/database.cpp
@@ -1,4 +1,20 @@
-#include "database.h"
+// database.h
+#pragma once
+#include <vector>
+#include <string>
+
+struct Genome {
+  std::string species;
+  std::string sequence;
+};
+
+class Database {
+public:
+  void load_from_file(const std::string& path);
+  std::vector<Genome> find_species(const std::string& name);
+private:
+  std::vector<Genome> genomes;
+};#include "database.h"
 #include <iostream>
 
 Database::Database(const std::string& path) {
