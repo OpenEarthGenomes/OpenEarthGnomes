b492beaee2fbbe2026ac5b813a256f92b748df40
Fáber Sándor <sandorwow@gmail.com>
2025-06-22 15:31:12 +0200

    Update database.h
    Signed-off-by: Fáber Sándor <sandorwow@gmail.com>

--- include/database.h
+++ include/database.h
@@ -6,6 +6,18 @@ class Database {
 public:
     Database(const std::string& path);
     ~Database();
+    bool execute(const std::string& sql);
+    
+private:
+    sqlite3* db = nullptr;
+};#pragma once
+#include <sqlite3.h>
+#include <string>
+
+class Database {
+public:
+    Database(const std::string& path);
+    ~Database();
     
     bool execute(const std::string& sql); // SQL parancs végrehajtása
     
