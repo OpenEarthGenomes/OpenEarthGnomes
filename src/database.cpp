8d1b65303a425405ed5820178c214ac7ae27010f
Fáber Sándor <sandorwow@gmail.com>
2025-06-22 16:01:39 +0200

    Update database.cpp
    Signed-off-by: Fáber Sándor <sandorwow@gmail.com>

--- src/database.cpp
+++ src/database.cpp
@@ -21,6 +21,29 @@ bool Database::execute(const std::string& sql) {
         return false;
     }
     return true;
+}#include "database.h"
+#include <iostream>
+
+Database::Database(const std::string& path) {
+    if(sqlite3_open(path.c_str(), &db) != SQLITE_OK) {
+        std::cerr << "Cannot open database: " << sqlite3_errmsg(db) << std::endl;
+    }
+}
+
+Database::~Database() {
+    sqlite3_close(db);
+}
+
+bool Database::execute(const std::string& sql) {
+    char* errMsg = nullptr;
+    int rc = sqlite3_exec(db, sql.c_str(), nullptr, nullptr, &errMsg);
+    
+    if(rc != SQLITE_OK) {
+        std::cerr << "SQL error: " << errMsg << std::endl;
+        sqlite3_free(errMsg);
+        return false;
+    }
+    return true;
 }// database.h
 #pragma once
 #include <vector>
