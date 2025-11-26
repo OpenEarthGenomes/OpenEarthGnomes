62d3f8af6a61ac9ea4408e3096632dd2c4792cc1
Fáber Sándor <sandorwow@gmail.com>
2025-11-26 04:35:00 +0100

    Refactor GenomeLoader interface and methods
    Signed-off-by: Fáber Sándor <sandorwow@gmail.com>

--- GenomeLoader.h
+++ GenomeLoader.h
@@ -1,26 +1,8 @@
 #pragma once
-#include <fstream>
-#include <sstream>
 #include <string>
 
 class GenomeLoader {
 public:
-    std::string loadFromFile(const std::string& filePath);
-    void saveToFile(const std::string& filePath, const std::string& genomeData);
+    std::string loadFromFile(const std::string& filepath);
+    bool saveToFile(const std::string& filepath, const std::string& genome_data);
 };
-
-// Implementáció
-std::string GenomeLoader::loadFromFile(const std::string& filePath) {
-    std::ifstream file(filePath, std::ios::binary);
-    if (!file) throw std::runtime_error("Fájl megnyitása sikertelen: " + filePath);
-    
-    std::ostringstream ss;
-    ss << file.rdbuf();
-    return ss.str();
-}
-
-void GenomeLoader::saveToFile(const std::string& filePath, const std::string& genomeData) {
-    std::ofstream file(filePath);
-    if (!file) throw std::runtime_error("Fájl létrehozása sikertelen: " + filePath);
-    file << genomeData;
-}
